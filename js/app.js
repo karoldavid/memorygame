bb4dd71e05d94e88adae98dcc36276c14f6a8ec3
karoldavid <k.zysk@zoho.com>
2017-03-10 13:38:05 +0100

    feat: show second card for a brief moment if no match

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ model.makeCards = function() {
 	var cards = [];
 	var number = this.size * this.size;
 	var allCardTypes = [];
+
 	model.symboles.forEach(function(symbol) {
 		allCardTypes.push(symbol, symbol);
 	});
@@ -78,8 +79,6 @@ controller.isCardSelected = function() {
 	return model.selectedCard != null;
 };
 
-
-
 var memoryGame = {};
 
 memoryGame.init = function(size) {
@@ -89,7 +88,7 @@ memoryGame.init = function(size) {
 	// this.tileElem = '<div class="card blue rounded-corners"></div>';
 	this.tileElem = '<div class="col s2"><div id="#" class="card-panel teal valign-wrapper"><i class="material-icons valign">add</i></div></div>';
 	this.size = size;
-	this.render();
+	this.renderBoard();
 	this.$cardElem = $('.card-panel');
 	this.$cardElem.click(function(event) {
 		var cardIndex = event.target.id || $(event.target).parent()[0].id;
@@ -98,9 +97,10 @@ memoryGame.init = function(size) {
 };
 
 memoryGame.showCard = function(cardIndex) {
+	var self = this;
 	var show = controller.getCardStatus(cardIndex);
 	var card = controller.getCard(cardIndex);
-	console.log(show);
+
 	if ((show && !controller.isCardSelected()) || (show && card.type === controller.getSelected().type))  {
 
 		this.$cardElem = $('#' + cardIndex + " .material-icons:first");
@@ -108,15 +108,20 @@ memoryGame.showCard = function(cardIndex) {
 		controller.setCardToVisible(cardIndex);
 
 	} else if (show && card.type != controller.getSelected().type){
-		this.$cardElem = $('#' + cardIndex + " .material-icons:first");
-		this.$cardElem.text('add');
-		controller.setCardToHidden(cardIndex);
-
 		var card2Index = controller.getSelected().id;
-		this.$cardElem = $('#' + card2Index + " .material-icons:first");
-		this.$cardElem.text('add');
-
+		var card2 = controller.getCard(card2Index);
+		controller.setCardToHidden(cardIndex);
 		controller.setCardToHidden(card2Index);
+
+		self.$cardElem = $('#' + cardIndex + " .material-icons:first");
+		self.$card2Elem = $('#' + card2Index + " .material-icons:first");
+
+		self.$cardElem.text(card.type);
+
+		setTimeout(function() {
+			self.$cardElem.text('add');
+			self.$card2Elem.text('add');
+		}, 250);
 	}
 	controller.isCardSelected() ? controller.setSelected(null) : controller.setSelected(card);
 };
@@ -136,14 +141,10 @@ memoryGame.makeRow = function(rowIndex){
 	return row;
 }
 
-memoryGame.buildBoard = function(){
+memoryGame.renderBoard = function(){
 	for(var i = 0; i < this.size; i++) {
 		this.$board.append(this.makeRow(i));
 	}
-};
-
-memoryGame.render = function() {
-	this.buildBoard();
 };
 
 controller.init();
