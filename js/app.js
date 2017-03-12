8db4dc53d3288f4a4926a2153bf6d003115e45f3
karoldavid <k.zysk@zoho.com>
2017-03-12 19:08:29 +0100

    refactor: encapsulate script in IIFE, and invoke strict mode

--- js/app.js
+++ js/app.js
@@ -1,300 +1,303 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+(function() {
+  "use strict";
 
-var model = {};
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-model.init = function() {
-	this.size = 4;
-	this.selectedCard = null;
-	this.seconds = 0;
-	this.moveCount = 0;
-	this.starRatingRemove = [10,14];
-	this.stars = 3;
-	this.finalStars = model.stars;
-	this.win = false;
-	this.symboles = ["android", "star", "call", "radio", "vpn_key", "work", "https", "videocam"];
-	this.cards = this.makeCards();
-};
+  var model = {};
 
-model.makeCards = function() {
-	var cards = [];
-	var number = this.size * this.size;
-	var allCardTypes = [];
+  model.init = function() {
+    this.size = 4;
+    this.selectedCard = null;
+    this.seconds = 0;
+    this.moveCount = 0;
+    this.starRatingRemove = [10, 14];
+    this.stars = 3;
+    this.finalStars = model.stars;
+    this.win = false;
+    this.symboles = ["android", "star", "call", "radio", "vpn_key", "work", "https", "videocam"];
+    this.cards = this.makeCards();
+  };
 
-	model.symboles.forEach(function(symbol) {
-		allCardTypes.push(symbol, symbol);
-	});
+  model.makeCards = function() {
+    var cards = [];
+    var number = this.size * this.size;
+    var allCardTypes = [];
 
-	for (var i = 0; i < number; i++) {
-		var index = getRandomInt(0, allCardTypes.length);
-		var type = allCardTypes.splice(index, 1)[0];
+    model.symboles.forEach(function(symbol) {
+      allCardTypes.push(symbol, symbol);
+    });
 
-		cards.push({
-			type: type,
-			hidden: true,
-			id: i
-		});
-	}
-	return cards;
-};
+    for (var i = 0; i < number; i++) {
+      var index = getRandomInt(0, allCardTypes.length);
+      var type = allCardTypes.splice(index, 1)[0];
 
-var controller = {};
+      cards.push({
+        type: type,
+        hidden: true,
+        id: i
+      });
+    }
+    return cards;
+  };
 
-controller.init = function(size) {
-	model.init(size);
-	memoryGame.init(model.size);
-};
+  var controller = {};
 
-controller.getCardStatus = function(cardIndex){
-	return model.cards[cardIndex].hidden;
-};
+  controller.init = function(size) {
+    model.init(size);
+    memoryGame.init(model.size);
+  };
 
-controller.checkPair = function(cardIndex) {
-	var type = model.cards[cardIndex].type;
-	var pair = model.cards.filter(function(card) {
-		return card.type === type;
-	});
-	return pair[0].hidden || pair[1].hidden ? true : false;
-};
+  controller.getCardStatus = function(cardIndex) {
+    return model.cards[cardIndex].hidden;
+  };
 
-controller.checkWin = function() {
-	var solved = model.cards.filter(function(card) {
-		return !card.hidden;
-	});
+  controller.checkPair = function(cardIndex) {
+    var type = model.cards[cardIndex].type;
+    var pair = model.cards.filter(function(card) {
+      return card.type === type;
+    });
+    return pair[0].hidden || pair[1].hidden ? true : false;
+  };
 
-	if (solved.length === model.cards.length) {
-		model.win = true;
-		memoryGame.showModalWin(model.seconds, model.finalStars, model.stars);
-	}
-};
+  controller.checkWin = function() {
+    var solved = model.cards.filter(function(card) {
+      return !card.hidden;
+    });
 
-controller.setCardToVisible = function (cardIndex) {
-	model.cards[cardIndex].hidden = false;
-};
+    if (solved.length === model.cards.length) {
+      model.win = true;
+      memoryGame.showModalWin(model.seconds, model.finalStars, model.stars);
+    }
+  };
 
-controller.setCardToHidden = function (cardIndex) {
-	model.cards[cardIndex].hidden = true;
-};
+  controller.setCardToVisible = function(cardIndex) {
+    model.cards[cardIndex].hidden = false;
+  };
 
-controller.getCard = function(cardIndex) {
-	return model.cards[cardIndex];
-};
+  controller.setCardToHidden = function(cardIndex) {
+    model.cards[cardIndex].hidden = true;
+  };
 
-controller.setSelected = function (card) {
-	model.selectedCard = card;
-};
+  controller.getCard = function(cardIndex) {
+    return model.cards[cardIndex];
+  };
 
-controller.getSelected = function(card) {
-	return model.selectedCard;
-};
+  controller.setSelected = function(card) {
+    model.selectedCard = card;
+  };
 
-controller.isCardSelected = function() {
-	return model.selectedCard != null;
-};
+  controller.getSelected = function(card) {
+    return model.selectedCard;
+  };
 
-controller.writeTimer = function() {
-	if (!model.win) {
-		memoryGame.updateTimer(model.seconds++);
-    	clearInterval(controller.writeTimer);
-	}
-}
+  controller.isCardSelected = function() {
+    return model.selectedCard != null;
+  };
 
-controller.counter = setInterval(controller.writeTimer, 1000);
+  controller.writeTimer = function() {
+    if (!model.win) {
+      memoryGame.updateTimer(model.seconds++);
+      clearInterval(controller.writeTimer);
+    }
+  }
 
-controller.writeMoveCounter = function() {
-	model.moveCount++;
-	memoryGame.updateMoveCounter(model.moveCount);
-};
+  controller.counter = setInterval(controller.writeTimer, 1000);
 
-controller.writeStarRating = function() {
-	var number = model.starRatingRemove.length -1;
+  controller.writeMoveCounter = function() {
+    model.moveCount++;
+    memoryGame.updateMoveCounter(model.moveCount);
+  };
 
-	if (model.finalStars > 1) {
-		model.finalStars--;
-	}
-	
-	model.starRatingRemove.forEach(function(remove, i) {
-		if (model.moveCount >= remove) {
-			memoryGame.updateStarRating("", number - i);
-		}
-	});
-};
+  controller.writeStarRating = function() {
+    var number = model.starRatingRemove.length - 1;
 
-controller.resetGame = function() {
+    if (model.finalStars > 1) {
+      model.finalStars--;
+    }
 
-	model.init();
-	
-	memoryGame.hideCards();
-	memoryGame.updateTimer(model.seconds);
-	memoryGame.updateMoveCounter(model.moveCount);
+    model.starRatingRemove.forEach(function(remove, i) {
+      if (model.moveCount >= remove) {
+        memoryGame.updateStarRating("", number - i);
+      }
+    });
+  };
 
-	for (var i = 0; i < model.stars; i++) {
-		memoryGame.updateStarRating("star", i);
-	}
-};
+  controller.resetGame = function() {
 
-var memoryGame = {};
+    model.init();
 
-memoryGame.init = function(size) {
-	var self = this;
+    memoryGame.hideCards();
+    memoryGame.updateTimer(model.seconds);
+    memoryGame.updateMoveCounter(model.moveCount);
 
-	this.size = size;
-	this.numberOfCards = size * size;
+    for (var i = 0; i < model.stars; i++) {
+      memoryGame.updateStarRating("star", i);
+    }
+  };
 
-	this.$board = $('#memoryGame');
-	this.$timeNeeded = $('#timeNeeded');
-	this.$finalStars = $('#finalStars');
+  var memoryGame = {};
 
-	this.rowElem = '<div class="row">';
-	this.tileElem = '<div class="col s3"><div id="#" class="card-panel teal valign-wrapper"><i class="material-icons blue-text text-lighten-2 valign">add</i></div></div>';
-	this.buttonElem = '<div class="col s6 l3"><a id="reset" class="waves-effect waves-light btn"><i class="material-icons left"></i>RESET</a></div>';
-	this.utilitiesElem = '<div id="utilities" class="row"></div>';
-	this.timerElem = '<div class="col s6 l3"><p>Time <span id="timer">00</span></p></div>';
-	this.moveCounterElem = '<div class="col s6 l3"><p>Moves <span id="moveCounter">0</span></p></div>';
-	this.starRatingElem = '<div id="starRating" class="col s6 l3"><i class="material-icons star valign">star</i><i class="material-icons star valign">star</i><i class="material-icons star valign">star</i></div>';
+  memoryGame.init = function(size) {
+    var self = this;
 
-	this.renderBoard();
-	this.renderUtilities();
+    this.size = size;
+    this.numberOfCards = size * size;
 
-	this.$cardElem = $('.card-panel');
-	
-	this.$cardElem.click(function(event) {
-		var cardIndex = event.target.id || $(event.target).parent()[0].id;
-		self.showCard(cardIndex);
-	});
-	
-	this.$resetButton = $('#reset');
-	
-	this.$resetButton.click(function() {
-		controller.resetGame();
-	});
-};
+    this.$board = $('#memoryGame');
+    this.$timeNeeded = $('#timeNeeded');
+    this.$finalStars = $('#finalStars');
 
-memoryGame.showCard = function(cardIndex) {
-	var self = this;
-	var show = controller.getCardStatus(cardIndex);
-	var card = controller.getCard(cardIndex);
+    this.rowElem = '<div class="row">';
+    this.tileElem = '<div class="col s3"><div id="#" class="card-panel teal valign-wrapper"><i class="material-icons blue-text text-lighten-2 valign">add</i></div></div>';
+    this.buttonElem = '<div class="col s6 l3"><a id="reset" class="waves-effect waves-light btn"><i class="material-icons left"></i>RESET</a></div>';
+    this.utilitiesElem = '<div id="utilities" class="row"></div>';
+    this.timerElem = '<div class="col s6 l3"><p>Time <span id="timer">00</span></p></div>';
+    this.moveCounterElem = '<div class="col s6 l3"><p>Moves <span id="moveCounter">0</span></p></div>';
+    this.starRatingElem = '<div id="starRating" class="col s6 l3"><i class="material-icons star valign">star</i><i class="material-icons star valign">star</i><i class="material-icons star valign">star</i></div>';
 
-	if ((show && !controller.isCardSelected()) || (show && card.type === controller.getSelected().type))  {
+    this.renderBoard();
+    this.renderUtilities();
 
-		this.$cardElem = $('#' + cardIndex + " .material-icons:first");
-		this.$cardElem.text(card.type);
-		controller.setCardToVisible(cardIndex);
-		controller.isCardSelected() ? controller.setSelected(null) : controller.setSelected(card);
-		controller.writeMoveCounter();
-		controller.writeStarRating();
-		controller.checkWin();
+    this.$cardElem = $('.card-panel');
 
-	} else if (show && card.type != controller.getSelected().type){
-		var card2Index = controller.getSelected().id;
-		var card2 = controller.getCard(card2Index);
-		controller.setCardToHidden(cardIndex);
-		controller.setCardToHidden(card2Index);
+    this.$cardElem.click(function(event) {
+      var cardIndex = event.target.id || $(event.target).parent()[0].id;
+      self.showCard(cardIndex);
+    });
 
-		self.$cardElem = $('#' + cardIndex + " .material-icons:first");
-		self.$card2Elem = $('#' + card2Index + " .material-icons:first");
+    this.$resetButton = $('#reset');
 
-		self.$cardElem.text(card.type);
+    this.$resetButton.click(function() {
+      controller.resetGame();
+    });
+  };
 
-		controller.setSelected(null);
-		controller.writeMoveCounter();
-		controller.writeStarRating();
+  memoryGame.showCard = function(cardIndex) {
+    var self = this;
+    var show = controller.getCardStatus(cardIndex);
+    var card = controller.getCard(cardIndex);
 
-		setTimeout(function() {
-			self.$cardElem.text('add');
-			self.$card2Elem.text('add');
-		}, 250);
-	}
-};
+    if ((show && !controller.isCardSelected()) || (show && card.type === controller.getSelected().type)) {
 
-memoryGame.hideCards = function() {
-	for (var i = 0; i < this.numberOfCards; i++) {
-		this.$cardElem = $('#' + i + " .material-icons:first");
-		this.$cardElem.text('add');
-	}
-};
+      this.$cardElem = $('#' + cardIndex + " .material-icons:first");
+      this.$cardElem.text(card.type);
+      controller.setCardToVisible(cardIndex);
+      controller.isCardSelected() ? controller.setSelected(null) : controller.setSelected(card);
+      controller.writeMoveCounter();
+      controller.writeStarRating();
+      controller.checkWin();
 
-memoryGame.makeRow = function(rowIndex){
-	var row = this.rowElem;
-	var tile = this.tileElem;
-	var id;
+    } else if (show && card.type != controller.getSelected().type) {
+      var card2Index = controller.getSelected().id;
+      var card2 = controller.getCard(card2Index);
+      controller.setCardToHidden(cardIndex);
+      controller.setCardToHidden(card2Index);
 
-	for (var i = 0; i < this.size; i++) {
-		id = rowIndex * this.size + i;
-		row += tile.replace('#', id);
-	}
+      self.$cardElem = $('#' + cardIndex + " .material-icons:first");
+      self.$card2Elem = $('#' + card2Index + " .material-icons:first");
 
-	row += '</div>';
+      self.$cardElem.text(card.type);
 
-	return row;
-}
+      controller.setSelected(null);
+      controller.writeMoveCounter();
+      controller.writeStarRating();
 
-memoryGame.renderBoard = function(){
-	for(var i = 0; i < this.size; i++) {
-		this.$board.append(this.makeRow(i));
-	}
-};
+      setTimeout(function() {
+        self.$cardElem.text('add');
+        self.$card2Elem.text('add');
+      }, 250);
+    }
+  };
 
-memoryGame.renderUtilities = function() {
-	this.$board.append(this.utilitiesElem);
-	this.$gameUtilities = $('#utilities');
-	this.renderButton();
-	this.renderTimer();
-	this.renderMoveCounter();
-	this.renderStarRating();
-};
+  memoryGame.hideCards = function() {
+    for (var i = 0; i < this.numberOfCards; i++) {
+      this.$cardElem = $('#' + i + " .material-icons:first");
+      this.$cardElem.text('add');
+    }
+  };
 
-memoryGame.renderButton = function() {
-	this.$gameUtilities.append(this.buttonElem);
-};
+  memoryGame.makeRow = function(rowIndex) {
+    var row = this.rowElem;
+    var tile = this.tileElem;
+    var id;
 
-memoryGame.renderTimer = function() {
-	this.$gameUtilities.append(this.timerElem);
-	this.$timer = $('#timer');
-};
+    for (var i = 0; i < this.size; i++) {
+      id = rowIndex * this.size + i;
+      row += tile.replace('#', id);
+    }
 
-memoryGame.renderMoveCounter = function() {
-	this.$gameUtilities.append(this.moveCounterElem);
-	this.$moveCounter = $('#moveCounter');
-};
+    row += '</div>';
 
-memoryGame.renderStarRating = function() {
-	this.$gameUtilities.append(this.starRatingElem);
-	this.$starRating = $('#starRating .star');
-};
+    return row;
+  }
 
-memoryGame.updateTimer = function(seconds) {
-	this.$timer.text(seconds);
-};
+  memoryGame.renderBoard = function() {
+    for (var i = 0; i < this.size; i++) {
+      this.$board.append(this.makeRow(i));
+    }
+  };
 
-memoryGame.updateMoveCounter = function(moveCount) {
-	this.$moveCounter.text(moveCount);
-};
+  memoryGame.renderUtilities = function() {
+    this.$board.append(this.utilitiesElem);
+    this.$gameUtilities = $('#utilities');
+    this.renderButton();
+    this.renderTimer();
+    this.renderMoveCounter();
+    this.renderStarRating();
+  };
 
-memoryGame.updateStarRating = function(text, index) {
+  memoryGame.renderButton = function() {
+    this.$gameUtilities.append(this.buttonElem);
+  };
+
+  memoryGame.renderTimer = function() {
+    this.$gameUtilities.append(this.timerElem);
+    this.$timer = $('#timer');
+  };
+
+  memoryGame.renderMoveCounter = function() {
+    this.$gameUtilities.append(this.moveCounterElem);
+    this.$moveCounter = $('#moveCounter');
+  };
+
+  memoryGame.renderStarRating = function() {
+    this.$gameUtilities.append(this.starRatingElem);
+    this.$starRating = $('#starRating .star');
+  };
+
+  memoryGame.updateTimer = function(seconds) {
+    this.$timer.text(seconds);
+  };
+
+  memoryGame.updateMoveCounter = function(moveCount) {
+    this.$moveCounter.text(moveCount);
+  };
+
+  memoryGame.updateStarRating = function(text, index) {
     this.$starRating.get(index).innerHTML = text;
-};
+  };
 
-memoryGame.showModalWin = function(seconds, stars, maxStars) {
+  memoryGame.showModalWin = function(seconds, stars, maxStars) {
 
-	this.$timeNeeded.text(seconds);
-	this.$finalStars.text(stars + "/" + maxStars);
+    this.$timeNeeded.text(seconds);
+    this.$finalStars.text(stars + "/" + maxStars);
 
-	$('#modalWin').modal({
+    $('#modalWin').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
       inDuration: 300, // Transition in duration
       outDuration: 200, // Transition out duration
       startingTop: '4%', // Starting top style attribute
       endingTop: '10%', // Ending top style attribute
-      complete: function() { 
-      	controller.resetGame();
-       }
-    }
-  ).modal('open');
-};
+      complete: function() {
+        controller.resetGame();
+      }
+    }).modal('open');
+  };
 
-controller.init();
+  controller.init();
+})();
